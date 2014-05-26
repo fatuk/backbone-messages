d54940db65d0d79b8628a5a04b98e495d5dad180
fatuk <fatukk@gmail.com>
2014-05-26 09:29:33 +0400

    Add search filter proto

---
+++ js/search-filter.js
@@ -0,0 +1,66 @@
+var InputSearchView = Backbone.View.extend({
+    template: $('#searchInputTemplate').html(),
+    initialize: function() {
+        this.render();
+    },
+    events: {
+        'keyup #searchInput': 'filterList'
+    },
+    filterList: function(e) {
+        var filtered = itemCollection.where({
+            name: $(e.currentTarget).val()
+        });
+        console.log(filtered);
+    },
+    render: function() {
+        var rendered = Mustache.render(this.template, {});
+        this.$el.html(rendered);
+        $('.js-search').append(this.el);
+    }
+});
+
+var ItemCollection = Backbone.Collection.extend({
+    url: 'data/items.json',
+    initialize: function() {
+        this.fetch({
+            reset: true
+        });
+    }
+});
+
+// var FilteredListCollection = Backbone.Collection.extend({});
+// var filteredListCollection = new FilteredListCollection();
+
+var ItemView = Backbone.View.extend({
+    template: $('#listItemTemplate').html(),
+    tagName: 'li',
+    render: function() {
+        var rendered = Mustache.render(this.template, this.model.toJSON());
+        this.$el.html(rendered);
+        return this;
+    }
+});
+
+var ListView = Backbone.View.extend({
+    tagName: 'ul',
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        this.collection.on('reset', function() {
+            this.collection.each(function(item) {
+                var itemView = new ItemView({
+                    model: item
+                });
+                this.$el.append(itemView.render().el);
+            }, this);
+            $('.js-search').append(this.el);
+        }, this);
+    }
+});
+
+var itemCollection = new ItemCollection();
+var inputSearchView = new InputSearchView();
+var listView = new ListView({
+    collection: itemCollection
+});
